32cbb4684bb2f98c1dee6c3d54f92f26ee8aabc1
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2021-10-09 22:17:38 -0500

    working on input group to add a new chirp

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import { BiAt } from "react-icons/bi";
-
+import { GiHummingbird } from "react-icons/gi";
 
 
 const App = () => {
 
-  const [chirp, setChirp] = useState()
+  const [username, setUsername] = useState('')
+  const [message, setMessage] = useState('')
+  const [chirp, setChirp] = useState([])
 
+  const addChirp = (e) => {
+    e.preventDefault();
+    const newChirp = setChirp.slice();
+    newChirp.unshift(message);
+    setChirp(newChirp);
+    setMessage('');
+  }
 
 
   return (
     <>
-      <h1 className="m-3 mb-5 text-center">Chirper</h1>
+      <h1 className="m-3 mb-5 text-center">Chirper<GiHummingbird className="mx-2" /></h1>
+
+      <section className="border-rounded shadow mb-5 mx-auto" id="chirp-form">
+        <div className="input-group">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon1">@</span>
+          </div>
+          <input type="text" className="form-control" placeholder="Username" aria-label="Username" />
+        </div>
+
+        <div className="input-group">
+          <div className="input-group-prepend">
+            <span className="input-group-text">What's on your mind?</span>
+          </div>
+          <textarea className="form-control" aria-label="With textarea"></textarea>
+        </div>
+      </section>
+<hr></hr>
       <section>
-        
         <div className="card bg-light shadow mb-3 mx-auto">
-          <div className="card-header"><BiAt/>I_Am_Bennett</div>
+          <div className="card-header"><BiAt />I_Am_Bennett</div>
           <div className="card-body">
             <p>It just goes to show...</p>
           </div>
         </div>
         <div className="card bg-light shadow mb-3 mx-auto">
-          <div className="card-header"><BiAt/>Mike_Scarn45</div>
+          <div className="card-header"><BiAt />Mike_Scarn45</div>
           <div className="card-body">
             <p>I want people to be afraid of how much they love me.</p>
           </div>
         </div>
         <div className="card bg-light shadow mb-3 mx-auto">
-          <div className="card-header"><BiAt/>TommyTommyHavHav</div>
+          <div className="card-header"><BiAt />TommyTommyHavHav</div>
           <div className="card-body">
             <p>At the risk of bragging, one of the things I’m best at is riding coattails. Behind every successful man is me. Smiling and taking partial credit.</p>
           </div>
         </div>
       </section>
-
-
     </>
   )
 }
